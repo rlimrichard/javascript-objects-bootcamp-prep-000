a3ab5600832f5e0522877bc10f3bd58fe0753b9d
Richard Lim <rlim.richard+flatiron@gmail.com>
2017-09-12 15:16:03 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,9 +2,9 @@ var playlist = {'artist' : 'song'}
 
 function updatePlaylist(playlist1, artistName, songTitle)
 {
-    playlist1[artistName] = songTitle
+    return playlist1[artistName] = songTitle
 }
 
 function updatePlaylist(playlist1, artistName){
-    delete playlist1[artistName]
+    return delete playlist1[artistName]
 }
